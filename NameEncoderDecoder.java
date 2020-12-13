f6d7690f11f9ab047a372132112a5496ccc9c3b9
Romchansky <61800488+Romchansky@users.noreply.github.com>
2020-12-13 23:53:34 +0200

    Add files via upload

---
+++ NameEncoderDecoder.java
@@ -0,0 +1,38 @@
+public class NameEncoderDecoder {
+
+    public String encode(String name) {
+        String encodeName = name
+                .replace("e", "1")
+                .replace("u", "2")
+                .replace("i", "3")
+                .replace("o", "4")
+                .replace("a", "5");
+        return "NOTFORYOU" + encodeName + "YESNOTFORYOU";
+    }
+
+    public String decode(String name) {
+        String decodeName = name
+                .replace("NOTFORYOU", "")
+                .replace("YES", "")
+                .replace("1", "e")
+                .replace("2", "u")
+                .replace("3", "i")
+                .replace("4", "o")
+                .replace("5", "a");
+        return decodeName;
+    }
+
+    public static void main(String[] args) {
+
+        NameEncoderDecoder nameEncoderDecoder = new NameEncoderDecoder();
+
+        String nameEncode = nameEncoderDecoder.encode("Crab");
+        System.out.println("nameEncoderDecoder.encode(\"Crab\") " + nameEncode);
+
+        String nameDecode = nameEncoderDecoder.decode(nameEncode);
+        System.out.println("nameEncoderDecoder.decode(nameEncode)" + nameDecode);
+
+
+    }
+}
+
